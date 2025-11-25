6b553897ac2c8343e861ae9cf54dbda63c9084c6
Yenuli Minsadi <samararatney@gmail.com>
2025-11-25 08:54:25 +0530

    update display of result

--- calculator.js
+++ calculator.js
@@ -3,26 +3,28 @@ const num1 = parseFloat(prompt("Enter 1st number "))
 const num2 = parseFloat(prompt("Enter 2nd number "))
 const sign = prompt("Enter sign ")
 
-let result = 0
+let result = 0;
+let valid = true;
 switch (sign) {
     case "+":
         result = num1 + num2;
-        console.log(result);
         break;
     case "-":
         result = num1 - num2;
-        console.log(result);
         break;
     case "*":
         result = num1 * num2;
-        console.log(result);
         break;
     case "/":
         result = num1 / num2;
-        console.log(result);
         break;
     case "%":
         result = num1 % num2;
-        console.log(result);
+        break;
+    default:
+        console.log("Invalid input");
+        valid = false;
         break;
 }
+
+if (valid) console.log(num1, sign, num2, "=" , result);
